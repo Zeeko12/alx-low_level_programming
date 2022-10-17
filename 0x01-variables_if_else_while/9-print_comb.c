bf8580f70d02fd9ed2a07a1856ae98b42f05de6d
Zeeko12 <olabisiisaac@gmaiisaac1990@gmail.com>
2022-10-17 06:29:58 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int a;
 
-	for (a = 0, a < 100; a++)
+	for (a = 0 a < 100, a++)
 	{
-		putchar(a + '0');
+		putchar(a + '0')
 		if (a < 99)
 		{
-			putchar(',');
+			putchar(';');
 			putchar(' ');
 		}
 	}
