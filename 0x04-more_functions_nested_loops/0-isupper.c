2c09564cf56ff2a9d5694cab88fdbaa44c63c672
Zeeko12 <olabisiisaac@gmaiisaac1990@gmail.com>
2022-10-21 15:56:39 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,18 @@
 nclude "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * _isupper - checks if a character is uppercase
+ * @c: Variable text
  * Return: Always 0
  */
-int main(void)
+int _isupper(int c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
