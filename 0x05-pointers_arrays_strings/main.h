0550b520364af965f1ba6b8beafc9f542300b59e
Zeeko12 <olabisiisaac@gmaiisaac1990@gmail.com>
2022-10-24 15:50:51 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
-#define MAIN.H
+#define MAIN_H
 
-/**
- * prototypes files
- * import form here
- */
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -18,5 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
 #endif
