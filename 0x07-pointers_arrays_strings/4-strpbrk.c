de394fb027e459cfdccf1fe4aad6196d461d77bd
Zeeko12 <olabisiisaac@gmaiisaac1990@gmail.com>
2022-11-01 15:25:56 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,8 +6,6 @@
  * @accept: The set of bytes to be searched for.
  *
  * Return: If a set is matched - a pointer to the matched byte.
- *
- * Return: If a set is matched - a pointer to the matched byte.
  * If no set is matched - NULL.
  */
 char *_strpbrk(char *s, char *accept)
